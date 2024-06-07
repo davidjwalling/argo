b719af59cf10d7b4f1a5cbb38399a2cda73eb212
davidjwalling <david@davidjwalling.com>
2024-06-07 12:37:17 -0500

    update

--- api.h
+++ api.h
@@ -6,6 +6,16 @@
 #include <WinSock2.h> // SOCKET, DWORD, va_list, vsnprintf
 #include <ws2ipdef.h> // sockaddr_in6
 #include <WS2tcpip.h> // inet_pton
+#else
+#if defined(__linux__) || defined (__APPLE__)
+#include <fcntl.h> // open
+#include <netinet/in.h> // sockaddr_in
+#include <signal.h> // signal
+#endif
+#include <arpa/inet.h> // inet_pton
+#include <cstdarg> // va_start, va_end
+#include <cstring> // memset
+#include <unistd.h> // fork
 #endif
 #include <cstdint> // uint16_t
 
@@ -16,8 +26,22 @@ enum {
 
 #if defined(_WIN32)
 #define WINSOCKVERSION 0x0202
+#else
+#if defined(__linux__) || defined(__APPLE__)
+#define closesocket(x) ::close(x)
+#endif
+#define INVALID_SOCKET (-1)
+#define SOCKET_ERROR (-1)
+#define WSAENOBUFS ENOBUFS
+#define WSAENOTCONN ENOTCONN
+#define WSAEWOULDBLOCK EWOULDBLOCK
+#define WSAGetLastError() errno
 #endif
 
 #if defined(_WIN32)
 using ssize_t = int;
+#else
+#if defined(__linux__)
+using SOCKET = size_t;
+#endif
 #endif

13d0bf1d5eeed8e446d6b7311cd9cc322b1c5985
davidjwalling <david@davidjwalling.com>
2024-06-12 18:37:37 -0500

    update

--- android/app/src/main/cpp/native-lib.cpp
+++ android/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,25 @@
+#include "idriver.h"
+
 #include <jni.h>
 #include <string>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_davidjwalling_argo_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+IDriver* driver = nullptr;
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_davidjwalling_argo_MainActivity_start(JNIEnv* env, jobject)
+{
+    (void)env;
+    driver = TheDriverPtr();
+    if (driver) {
+        driver->Start(0, NULL);
+    }
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_davidjwalling_argo_MainActivity_stop(JNIEnv* env, jobject)
+{
+    (void)env;
+    if (driver) {
+        driver->Stop();
+    }
 }
--- android/app/src/main/java/com/davidjwalling/argo/MainActivity.java
+++ android/app/src/main/java/com/davidjwalling/argo/MainActivity.java
@@ -3,15 +3,20 @@ package com.davidjwalling.argo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.KeyEvent;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.EditText;
 import java.io.File;
 
 import com.davidjwalling.argo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'argo' library on application startup.
+    static int maxLogLines = 2000;
+    static String firstLogLine = "I1001 Argo Experimental [0.X]";
+    static String newLine = "\n";
     static {
         try {
             System.loadLibrary("argo");
@@ -23,18 +28,71 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        //  Initialize the TextView.
 
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        final TextView tv = findViewById(R.id.textView);
+        tv.setMaxLines(maxLogLines);
+        tv.setMovementMethod(new ScrollingMovementMethod());
+        tv.setText(getString(R.string.text_hint, firstLogLine));
+
+        //  Handle EditText activity.
+
+        final EditText et = findViewById(R.id.editText);
+        et.requestFocus();
+        et.setOnEditorActionListener((v, actionId, event) -> {
+
+            //  We're only interested in Enter key press.
+
+            if (event.getAction() != KeyEvent.ACTION_DOWN)
+                return false;
+            if (event.getKeyCode() != KeyEvent.KEYCODE_ENTER)
+                return false;
+
+            //  Done if nothing was entered.
+
+            final String inp = et.getText().toString();
+            if (inp.isEmpty())
+                return true;
+
+            //  Fetch the TextView content and append the input.
+            //  If we were already at max-lines, remove the first.
+            //  Update the TextView.
+
+            String txt = tv.getText().toString() + newLine + inp;
+            if (tv.getLineCount() >= maxLogLines)
+                tv.setText(txt.substring(txt.indexOf(newLine)+1));
+            else
+                tv.setText(getString(R.string.text_hint, txt));
+
+            //  Clear the edit text.
+
+            et.setText("");
+            return true;
+        });
+
+        //start();
     }
 
-    /**
-     * A native method that is implemented by the 'argo' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stop();
+    }
+
+    public native void start();
+    public native void stop();
 }

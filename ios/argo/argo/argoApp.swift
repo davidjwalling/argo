19a78f4a583746f2274133e7ab0bdb7f47f8a063
davidjwalling <david@davidjwalling.com>
2024-06-26 08:35:09 -0500

    update

---
+++ ios/argo/argo/argoApp.swift
@@ -0,0 +1,17 @@
+//
+//  argoApp.swift
+//  argo
+//
+//  Created by Me on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct argoApp: App {
+    var body : some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
